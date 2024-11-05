7066e4409959de1241de418089041db85ea81b3d
Fabkovsky <115949931+Fabkovsky@users.noreply.github.com>
2024-11-05 10:30:21 +0100

    Layout change in MP

--- Vinted/Vinted/obj/Debug/net8.0-ios/iossimulator-x64/Vinted.AssemblyInfo.cs
+++ Vinted/Vinted/obj/Debug/net8.0-ios/iossimulator-x64/Vinted.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vinted")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2b39e5ed8ed3549caf2b30886e3d141ce770af01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+143c5e3b86965ae75b36aa52f79545726eb344d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vinted")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vinted")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
